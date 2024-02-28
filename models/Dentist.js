05164c41dd4f642bdc2d1dafc688528a68c4274d
Phatdanai Arsomngern <nongpepe@gmail.com>
2024-02-28 23:31:15 +0700

    model

--- models/Dentist.js
+++ models/Dentist.js
@@ -1,41 +1,20 @@
 const mongoose = require('mongoose');
 
-const HospitalSchema = new mongoose.Schema({
+const DentistSchema = new mongoose.Schema({
     name:{
         type: String ,
-        required:[true , 'Please add a neme '],
-        unique: true ,
+        required:[true , 'Please add a name '],
         trim : true ,
         maxlength:[50, 'Name can not be more than 50 characters']
     } ,
-    address:{
+    yearsOfExperience:{
         type: String,
-        required:[true, 'Please add an adress']
+        required:[true, 'Please add years of experience']
     },
-    district:{
+    areaOfExpertise:{
         type: String,
-        required:[true, 'Please add a district']
-    },
-
-    province:{
-        type: String,
-        required:[true, 'Please add a province']
-    },
-
-    postalcode:{
-        type: String,
-        required:[true, 'Please add an postalcode'] ,
-        maxlength:[5 , 'Postal Code can not be more than 5 digits']
-    },
-
-    tel:{
-        type: String
-    },
-
-    region:{
-        type: String,
-        required:[true, 'Please add a region']
-    },
+        required:[true, 'Please add an area of expertise']
+    }
 } ,
 
 {
@@ -45,19 +24,19 @@ const HospitalSchema = new mongoose.Schema({
 );
 
 //Reverse populate with virtuals
-HospitalSchema.virtual('appointments',{
+DentistSchema.virtual('appointments',{
     ref : 'Appointment',
     localField: '_id',
-    foreignField: 'hospital',
+    foreignField: 'dentist',
     justOne:false 
 }) ;
 
 //Cascade delete appointments when a hospital is deleted 
-HospitalSchema.pre('deleteOne' , {document: true , query :false} , async function(next){
+DentistSchema.pre('deleteOne' , {document: true , query :false} , async function(next){
     console.log(`Appointments being removed from hospital ${this._id}`) ;
     await this.model('Appointment').deleteMany({hospital: this._id}) ;
     next() ;
 }) ;
 
 
-module.exports=mongoose.model('Hospital' , HospitalSchema) ;
+module.exports=mongoose.model('Dentist' , DentistSchema) ;
