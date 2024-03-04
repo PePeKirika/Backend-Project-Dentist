fa949adc632068df35e0e7026af83319fc2359bd
Phatdanai Arsomngern <nongpepe@gmail.com>
2024-03-04 14:15:43 +0700

    201

--- controllers/dentists.js
+++ controllers/dentists.js
@@ -96,7 +96,7 @@ exports.getDentist =async( req , res , next)=>{
 
 exports.createDentist = async (req , res , next)=>{
     const dentist = await Dentist.create(req.body);
-    res.status(200).json({success:true, data:dentist});
+    res.status(201).json({success:true, data:dentist});
 };
 
 
