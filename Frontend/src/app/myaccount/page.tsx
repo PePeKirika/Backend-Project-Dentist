875845ca4d42bd12d7c58f67bb43fcb6870342ed
PePeKirika <nongpepe@gmail.com>
2024-03-21 23:22:34 +0700

    fix name

---
+++ Frontend/src/app/myaccount/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function MyAccount() {
+    return (
+        <main>
+            <div className="text2xl text-center text-white">My Account</div>
+        </main>
+    )
+}
