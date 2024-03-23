fe311ece7e02996ef07686f58c4e264ae1b11b8f
PePeKirika <nongpepe@gmail.com>
2024-03-24 01:31:23 +0700

    FEAT get all appointments

--- Frontend/interface.ts
+++ Frontend/interface.ts
@@ -1,22 +1,28 @@
-  interface DentistItem {
-    _id: string,
-    name: string,
-    yearsOfExperience:string,
-    areaOfExpertise:string,
-    picture: string, 
-  }
+interface DentistItem {
+  _id: string;
+  name: string;
+  yearsOfExperience: string;
+  areaOfExpertise: string;
+  picture: string;
+}
 
-  interface DentistJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: DentistItem[]
-  }
+interface DentistJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: DentistItem[];
+}
 
-  interface AppointmentItem {
-    appDate : string;
-    user : string;
-    userName :string;
-    dentist : string;
-    createAt : string;
-  }
+interface AppointmentItem {
+  appDate: Date;
+  user: string;
+  userName: string;
+  dentist: DentistItem[];
+  createAt: string;
+}
+
+interface AppointmentJson {
+  success: boolean;
+  count: number;
+  data: AppointmentItem[];
+}
---
+++ Frontend/src/libs/getAppointments.tsx
@@ -0,0 +1,19 @@
+export default async function getAppointments(token: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch(
+    "https://project-dentist-backend.vercel.app/api/v1/appointments",
+    {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Cannot get Appointments");
+  }
+
+  return await response.json();
+}
